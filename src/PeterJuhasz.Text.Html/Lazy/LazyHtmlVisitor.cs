3809d9346a8e475138a1f496ae1fd32f4aedaa34
Juhász Péter <peter.juhasz@live.com>
2026-09-21 12:50:49 +0200

    visitor

---
+++ src/PeterJuhasz.Text.Html/Lazy/LazyHtmlVisitor.cs
@@ -0,0 +1,29 @@
+﻿namespace System.Text.Html.Lazy;
+
+public abstract class LazyHtmlVisitor
+{
+	public virtual void VisitDocument(LazyHtmlDocument document)
+	{
+		foreach (var element in document.Elements())
+		{
+			VisitElement(element);
+		}
+	}
+
+	public virtual void VisitElement(LazyHtmlElement element)
+	{
+		foreach (var attribute in element.Attributes())
+		{
+			VisitAttribute(element, attribute);
+		}
+
+		foreach (var child in element.Elements())
+		{
+			VisitElement(child);
+		}
+	}
+
+	public virtual void VisitAttribute(LazyHtmlElement element, LazyHtmlAttribute attribute)
+	{
+	}
+}
